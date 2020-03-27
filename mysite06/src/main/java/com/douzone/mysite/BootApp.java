99ed47fc760844db82c4ea8f7e788712591122a4
jun <jun@192.168.1.132>
2020-03-27 16:17:04 +0900

    mysite06 war 배포 업데이트

--- mysite06/src/main/java/com/douzone/mysite/BootApp.java
+++ mysite06/src/main/java/com/douzone/mysite/BootApp.java
@@ -3,11 +3,10 @@ package com.douzone.mysite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BootApp {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BootApp.class, args);
-	}
+	@SpringBootApplication
+	public class BootApp {
+		public static void main(String[] args) {
+			SpringApplication.run(BootApp.class, args);
+		}
 
 }
