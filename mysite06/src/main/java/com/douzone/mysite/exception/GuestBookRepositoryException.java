d3af7522aa9f7e5824281abc834a451d06773b3b
jun <jun@192.168.1.132>
2020-03-27 09:52:31 +0900

    gitignore 설정

---
+++ mysite06/src/main/java/com/douzone/mysite/exception/GuestBookRepositoryException.java
@@ -0,0 +1,11 @@
+package com.douzone.mysite.exception;
+
+public class GuestBookRepositoryException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public GuestBookRepositoryException() {
+		super("GuestBookRepositoryException Occurs");
+	}
+	public GuestBookRepositoryException(String Message) {
+		super(Message);
+	}
+}
