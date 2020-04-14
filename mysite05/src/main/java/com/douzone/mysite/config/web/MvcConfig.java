56468129f4d93a39fa0802f03025a3989ed8765e
JUNG JUN YONG <jkouese@gmail.com>
2020-04-14 14:09:52 +0900

    mysite05 update

--- mysite05/src/main/java/com/douzone/mysite/config/web/MvcConfig.java
+++ mysite05/src/main/java/com/douzone/mysite/config/web/MvcConfig.java
@@ -1,7 +1,17 @@
 package com.douzone.mysite.config.web;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,7 +22,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -21,11 +31,38 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setExposeContextBeansAsAttributes(true);
 		return viewResolver;
 	}
-	
+
 	// Default Servlet handler
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
+	}
+
+	// Message Converters
+	@Bean
+	public StringHttpMessageConverter StringHttpMessageConverter() {
+		StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+		messageConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "html", Charset.forName("utf-8"))));
+		return messageConverter;
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter MappingJackson2HttpMessageConverter() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder().indentOutput(true)
+				.dateFormat(new SimpleDateFormat("yyyy-mm-dd"));
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build());
+		messageConverter
+				.setSupportedMediaTypes(Arrays.asList(new MediaType("application", "json", Charset.forName("utf-8"))));
+
+		return messageConverter;
+	}
+	
+	
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(StringHttpMessageConverter());
+		converters.add(MappingJackson2HttpMessageConverter());
 	}
 
 	// Mvc Resources(URL magic Mapping)
@@ -33,6 +70,5 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**").addResourceLocations("file:/Users/jun/mysite-uploads/");
 	}
-	
-	
+
 }
