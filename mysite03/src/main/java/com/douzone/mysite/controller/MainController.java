d098f07686fbdff5d29dfe2e9d2e6f1471da3102
JUNG JUN YONG <jkouese@gmail.com>
2020-04-14 11:58:21 +0900

    json exception 응답

--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.douzone.mysite.service.MainService;
 import com.douzone.mysite.vo.MainVo;
+import com.douzone.mysite.vo.UserVo;
 
 
 @Controller
@@ -20,6 +22,12 @@ public class MainController {
 		MainVo mainVo = mainService.find();
 		model.addAttribute("vo",mainVo);
 		return "main/index";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public UserVo hello() {
+		return new UserVo();
 	}
 	
 	
